ddedd939e4f70b25f37c3978c0e8d64a0d881310
Lukas Zujevas <zujevasl@gmail.com>
2026-04-10 18:45:40 +0300

    feat: update button text color in HighlightCta component for better contrast

--- client/src/components/HighlightCta.vue
+++ client/src/components/HighlightCta.vue
@@ -12,7 +12,7 @@ defineProps({
 
 <template>
     <button
-        class="bg-primary-container font-label font-bold text-black bg-linear-to-tl from-primary-container from-50% to-primary px-6 py-3 text-center rounded-sm" 
+        class="bg-primary-container font-label font-bold text-on-primary bg-linear-to-tl from-primary-container from-50% to-primary px-6 py-3 text-center rounded-sm" 
         @click="console.log('clicked')"
     >
         {{ text }}
