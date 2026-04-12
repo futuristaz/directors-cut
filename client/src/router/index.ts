b11d2b6d15535ed2d3fb52029cf53339218ff6e9
Lukas Zujevas <zujevasl@gmail.com>
2026-04-12 23:45:32 +0300

    feat: add NotFoundView component and update router to include 404 route

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,18 +1,14 @@
 import SignupView from '@/views/SignupView.vue'
 import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      component: HomeView
-    },
-    {
-      path: '/signup',
-      component: SignupView
-    }
+    { path: '/',name: 'LandingHome', component: HomeView },
+    { path: '/signup', name: 'Signup', component: SignupView },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView }
   ],
 })
 
---
+++ client/src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="bg-surface">
+        <h1 class="font-headline text-5xl font-bold text-primary-container mt-10 text-center">
+            This is a 404 page
+        </h1>
+    </div>
+</template>
