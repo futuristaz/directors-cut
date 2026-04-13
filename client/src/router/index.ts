2d0b3cdc9bd9ad2b068321d2d6f39ec39d36ad9c
Lukas Zujevas <zujevasl@gmail.com>
2026-04-13 15:49:08 +0300

    feat: add LoginForm component and integrate it into LoginView with routing

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,12 +2,14 @@ import SignupView from '@/views/SignupView.vue'
 import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import NotFoundView from '@/views/NotFoundView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/',name: 'LandingHome', component: HomeView },
     { path: '/signup', name: 'Signup', component: SignupView },
+    { path: '/login', name: 'Login', component: LoginView },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView }
   ],
 })
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import LoginForm from '@/components/login/LoginForm.vue';
+
+</script>
+
+<template>
+    <LoginForm />
+</template>
